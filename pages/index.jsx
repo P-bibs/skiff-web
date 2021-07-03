a26e22b8e6c3b680053463612c1959ac32ea6ef6
Paul Biberstein <biberstein.paul@gmail.com>
2021-07-03 09:53:36 -0400

    Use example program on startup

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,7 +8,7 @@ const Evaluator = dynamic(() => import("../components/Evaluator"), {
 });
 
 export default function Home() {
-  const [editorRef, state] = useCodeMirror({});
+  const [editorRef, state] = useCodeMirror({ doc: exampleProgram });
 
   return (
     <>
@@ -31,3 +31,48 @@ export default function Home() {
     </>
   );
 }
+
+const exampleProgram = `
+data List:
+	| link(first,rest)
+	| empty()
+end
+let mt = empty()
+
+data Option:
+    | some(v)
+    | none()
+end
+
+def singles(lst, to_replace):
+    match to_replace:
+        | some(v) => 
+            match lst:
+                | link(f,r) => 
+                    if f == v:
+                        singles(r, to_replace)
+                    else:
+                        link(f, singles(r, some(f)))
+                    end
+                | empty() => empty()
+            end
+        | none() =>
+            match lst:
+                | link(f,r) => link(f, singles(r, some(f)))
+                | empty() => empty()
+            end
+    end
+end
+
+let one_through_four = link(1,link(2,link(3,link(4,mt))))
+let with_dups1 = link(1,link(1,link(2,link(2,link(3,link(4,mt))))))
+let with_dups2 = link(1,link(2,link(3,link(3,link(3,link(4,mt))))))
+
+singles(mt, none()) == mt
+
+singles(one_through_four, none()) == one_through_four
+
+singles(with_dups1, none()) == one_through_four
+
+singles(with_dups2, none()) == one_through_four
+`;
