50ac28866d093bbf0b73daa46d68747fe920b196
Paul Biberstein <biberstein.paul@gmail.com>
2021-09-06 20:06:52 -0400

    Add Github link

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,15 @@ const Evaluator = dynamic(() => import("../components/Evaluator"), {
 });
 
 export default function Home() {
-  const [editorRef, state, view] = useCodeMirror({ doc: examplePrograms["Language Tour"] });
+  const [editorRef, state, view] = useCodeMirror({
+    doc: examplePrograms["Language Tour"],
+  });
 
   const setEditorContent = (text) => {
-    view.current.dispatch({changes: {from: 0, to: state.doc.length, insert: text}})
-  }
+    view.current.dispatch({
+      changes: { from: 0, to: state.doc.length, insert: text },
+    });
+  };
 
   return (
     <>
@@ -27,8 +31,23 @@ export default function Home() {
         className="flex flex-col"
       >
         <div className="w-full h-10 px-3 bg-gray-800 text-white flex flex-row items-center justify-between">
-          <div className="py-2">Skiff</div>
-          <ExamplePicker setEditorContent={setEditorContent}/>
+          <div className="py-2 flex flex-row items-center justify-center">
+            Skiff
+            <a
+              className="text-black"
+              href="https://github.com/p-bibs/skiff"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                alt="Skiff on Github"
+                className="w-4 h-4 m-3 inline "
+                style={{ filter: "brightness(0) invert(1)" }}
+                src={"/githubicon.png"}
+              />
+            </a>
+          </div>
+          <ExamplePicker setEditorContent={setEditorContent} />
         </div>
         <div className="w-full h-0 flex-grow flex flex-row">
           <Editor editorRef={editorRef} />
